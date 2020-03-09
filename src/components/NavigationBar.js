40dafe72345a2e971bf723add67b8dc744c795f6
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-09 20:53:49 +1100

    trying to fix navbar styling

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 const Styles = styled.div`
   .navbar {
     background-color: #222;
+    height: 8vh;
   }
+
   .navbar-brand {
     font-weight: 600;
     color: #ffa8ca;
@@ -16,6 +19,7 @@ const Styles = styled.div`
       color: #ffa8ca;
     }
   }
+  
   .navbar-nav .nav-link {
     color: #9bf2d7;
     font-weight: 400;
@@ -26,43 +30,53 @@ const Styles = styled.div`
   }
 `;
 export const NavigationBar = () => (
-  <Styles>
-    <Navbar expand="lg">
-      <Navbar.Brand>
-        <Link to="/">Sweets To Go</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/menu">Menu</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/login">Login</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/account">Account</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/purchase">
-                <i className="fas fa-shopping-cart" />
-              </Link>
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </Styles>
-);
+         <Styles>
+           <Navbar expand="lg">
+             <Navbar.Brand>
+               <Link className="navbar-brand" to="/">
+                 Sweets To Go
+               </Link>
+             </Navbar.Brand>
+             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+             <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="ml-auto">
+                 <Nav.Item>
+                   <Nav.Link>
+                     <Link className="nav-link" to="/">
+                       Home
+                     </Link>
+                   </Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                   <Nav.Link>
+                     <Link className="nav-link" to="/menu">
+                       Menu
+                     </Link>
+                   </Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                   <Nav.Link>
+                     <Link className="nav-link" to="/login">
+                       Login
+                     </Link>
+                   </Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                   <Nav.Link>
+                     <Link className="nav-link" to="/account">
+                       Account
+                     </Link>
+                   </Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                   <Nav.Link>
+                     <Link className="nav-link" to="/purchase">
+                       <i className="fas fa-shopping-cart" />
+                     </Link>
+                   </Nav.Link>
+                 </Nav.Item>
+               </Nav>
+             </Navbar.Collapse>
+           </Navbar>
+         </Styles>
+       );
