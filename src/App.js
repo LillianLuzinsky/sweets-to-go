61fe2880ea9a0be16cf1b6f069ed3b014e29e39a
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-09 15:12:23 +1100

    fixed navbar links

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {SummerHome} from './components/Home/SummerHome';
-import {Login} from './components/Login/Login';
-import {Signup} from './components/Signup/Signup';
-import {NoMatch} from './components/NoMatch';
-import {NavigationBar} from './components/NavigationBar';
-import MenuPage from './components/Menu/MenuPage';
-import PurchasePage from './components/Cart/PurchasePage';
-import AccountPage from './components/Account/AccountPage';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { SummerHome } from "./components/Home/SummerHome";
+import { Login } from "./components/Login/Login";
+import { Signup } from "./components/Signup/Signup";
+import { NoMatch } from "./components/NoMatch";
+import { NavigationBar } from "./components/NavigationBar";
+import MenuPage from "./components/Menu/MenuPage";
+import PurchasePage from "./components/Cart/PurchasePage";
+import AccountPage from "./components/Account/AccountPage";
 class App extends Component {
-  render(){
+  render() {
     return (
       <React.Fragment>
-        <NavigationBar/>
-          <Router>
-            <Switch>
-            <Route exact path="/sweets-to-go/" component={SummerHome} />
-              <Route path="/menu" component={MenuPage} />
-              <Route path="/purchase" component={PurchasePage} />
-              <Route path="/login" component={Login} />
+        <Router basename="/sweets-to-go">
+          <NavigationBar />
+          <Switch>
+            <Route exact path="/" component={SummerHome} />
+            <Route path="/menu" component={MenuPage} />
+            <Route path="/purchase" component={PurchasePage} />
+            <Route path="/login" component={Login} />
             <Route path="/account" component={AccountPage} />
             <Route path="/signup" component={Signup} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </React.Fragment>
     );
-  } 
+  }
 }
-
-
 export default App;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,59 +1,68 @@
-import React from 'react';
-import {Nav, Navbar} from 'react-bootstrap';
-import styled from 'styled-components';
-
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Styles = styled.div`
   .navbar {
     background-color: #222;
   }
-
   .navbar-brand {
     font-weight: 600;
     color: #ffa8ca;
     font-size: 1.5rem;
     padding-left: 2rem;
     letter-spacing: 1px;
-
     &:hover {
       color: #ffa8ca;
     }
   }
-
   .navbar-nav .nav-link {
     color: #9bf2d7;
     font-weight: 400;
     padding: 1rem 2rem;
-
     &:hover {
       color: white;
     }
   }
 `;
-
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Sweets To Go</Navbar.Brand>
+      <Navbar.Brand>
+        <Link to="/">Sweets To Go</Link>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/menu">Menu</Nav.Link>
+            <Nav.Link>
+              <Link to="/menu">Menu</Link>
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link>
+              <Link to="/login">Login</Link>
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/account">Account</Nav.Link>
+            <Nav.Link>
+              <Link to="/account">Account</Link>
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/purchase"><i className="fas fa-shopping-cart" /></Nav.Link>
+            <Nav.Link>
+              <Link to="/purchase">
+                <i className="fas fa-shopping-cart" />
+              </Link>
+            </Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   </Styles>
-  );
+);
